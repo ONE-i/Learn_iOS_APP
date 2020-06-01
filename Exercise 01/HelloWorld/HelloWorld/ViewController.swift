7e4701f8a5c22705eaf4f0b831184b1529a0eefa
ONE-i <chonhungdieunhamnhi@gmail.com>
2020-05-31 22:25:58 -0400

    Organize folders

---
+++ Exercise 01/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by ØNE on 5/27/20.
+//  Copyright © 2020 ØNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exercise 03/SimpleLayout_2/SimpleLayout_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleLayout_2
+//
+//  Created by ØNE on 5/31/20.
+//  Copyright © 2020 ØNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
