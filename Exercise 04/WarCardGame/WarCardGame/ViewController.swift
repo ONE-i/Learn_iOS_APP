df3f8681385403d2eb358b677664e56b99c5c86b
ONE-i <chonhungdieunhamnhi@gmail.com>
2020-06-01 23:22:27 -0400

    Created War Card Game

---
+++ Exercise 04/WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by ØNE on 6/1/20.
+//  Copyright © 2020 ØNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
